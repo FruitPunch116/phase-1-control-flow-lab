dc947c2e99a05cebdf4a32cde757935e2db830c0
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-06-14 10:41:08 -0400

    Revert "Code"
    This reverts commit cd0aec5362760a68bf437575e3698d1bea16284b.

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-function scuberGreetingForFeet(distanceInFeet){
-  // Write your code here! 
-  if (distanceInFeet <= 400 ) {
-    return "This one is on me!";
-  
-  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
-    return "That will be twenty bucks.";
-  
-  } else if (distanceInFeet > 2000 && distanceInFeet <2500) {
-    return "I will gladly take your thirty bucks.";
-  
-  } else {
-    return "No can do.";
-  }
+function scuberGreetingForFeet(){
+  // Write your code here!
 }
 
-function ternaryCheckCity(city){
+function ternaryCheckCity(){
   // Write your code here!
-  return city === "NYC" ? "Ok, sounds good." : "No go."
-  // city = "Pittsburgh" ? "No go." : "Ok, sounds good."
 }
 
-function switchOnCharmFromTip(tip){
+function switchOnCharmFromTip(){
   // Write your code here!
-  const greeting = tip;
-  switch (greeting) {
-  case "generous" :
-    return "Thank you so much.";
-  case "not as generous" :
-    return "Thank you.";
-  default:
-    return "Bye.";
-  }
 }

eab897e602b9587fe5550c0387c5ef2aed03b1c7
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-06-15 10:36:48 -0400

    Conditionals

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let greeting
+  if (feet <= 400){
+    return greeting = "This one is on me!"
+  } else if (feet >= 400 && feet <=2000){
+    return greeting = "That will be twenty bucks."
+  } else if (feet >=2001 && feet < 2500){
+    return greeting = "I will gladly take your thirty bucks."
+  } else if (feet >= 2500){
+    return greeting = "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let greeting
+  city == "NYC" ? greeting = "Ok, sounds good." : greeting =  "No go."
+  return greeting
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let greeting
+  switch (tip) {
+    case "generous":
+      greeting ="Thank you so much."
+    break;
+    case "not as generous":
+      greeting = "Thank you."
+    break;
+    default:
+      greeting = "Bye."
+  }
+  return greeting
 }
